819d9c3ea470ad3d37de4f48c2291f5978733410
YY_holitel <your email>
2026-05-14 09:06:31 +0800

    添加音乐和屏幕自适应功能

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,11 +2,16 @@ package app;
 
 import ui.InitFrame;
 import ui.UiFont;
+import utils.RandomMusicPlayer;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        RandomMusicPlayer musicPlayer = RandomMusicPlayer.fromDefaultMusicDir();
+        musicPlayer.startLoop();
+        Runtime.getRuntime().addShutdownHook(new Thread(musicPlayer::stop, "music-player-shutdown"));
+
         UiFont.installDefaults();
         SwingUtilities.invokeLater(InitFrame::new);
     }
